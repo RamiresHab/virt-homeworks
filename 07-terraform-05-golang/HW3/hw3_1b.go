0b450c88f415458a643ec3e2a4ebefd8e2972a95
Roman Khabibullin <roman-habibullin@yandex.ru>
2022-11-14 12:39:58 +0300

    files for 3.1 answer

---
+++ 07-terraform-05-golang/HW3/hw3_1b.go
@@ -0,0 +1,9 @@
+    package main
+       
+    import "fmt"
+    
+    func main() {
+		var metres float64 = 64.5
+        var foots float64 = metres / 0.3048  
+        fmt.Println("In", metres, "metres", foots, "foots")    
+    }
